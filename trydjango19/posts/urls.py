c093b7a6a2a6260936cf6d4c5cb9970b0a3513e3
viren <vickyspatel@gmail.com>
2019-11-07 13:26:11 +0530

    Added new Views and Created URL patterns

---
+++ trydjango19/posts/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import url
+from .views import (posts_home,
+                    post_create,
+                    post_delete,
+                    post_detail,
+                    post_list,
+                    post_update
+                    )
+
+# Write your URL patterns here.
+urlpatterns = [
+    url(r'^$',  posts_home, name="home"),
+    url(r'^create', post_create, name="post-create"),
+    url(r'^delete', post_delete, name="post-delete"),
+    url(r'^detail', post_detail, name="post-detail"),
+    url(r'^list', post_list, name="post-list"),
+    url(r'^update', post_update, name="post-update"),
+]
--- trydjango19/posts/views.py
+++ trydjango19/posts/views.py
@@ -5,4 +5,24 @@ from django.http import HttpResponse
 
 
 def posts_home(request):
-    return HttpResponse("<h1>Hello WOrld!!</h1>")
+    return HttpResponse("<h1>Hello World!!</h1>")
+
+
+def post_create(request):
+    return HttpResponse("<h1>Create Post!!</h1>")
+
+
+def post_detail(request):
+    return HttpResponse("<h1>Detail</h1>")
+
+
+def post_list(request):
+    return HttpResponse("<h1>List</h1>")
+
+
+def post_update(request):
+    return HttpResponse("<h1>Update</h1>")
+
+
+def post_delete(request):
+    return HttpResponse("<h1>Delete</h1>")
