e543281073eaa369e908bd23ef7975090eb162d7
vr3n <vickyspatel@gmail.com>
2019-11-08 23:55:31 +0530

    Added StackOverflow like input Functionality.

--- trydjango19/posts/forms.py
+++ trydjango19/posts/forms.py
@@ -1,13 +1,15 @@
 from django import forms
-
+from pagedown.widgets import PagedownWidget
 from .models import Post
 
 # Create your forms here.
 
+
 # Form for creating Post.
-
-
 class PostForm(forms.ModelForm):
+    content = forms.CharField(widget=PagedownWidget)
+    publish = forms.DateField(widget=forms.SelectDateWidget)
+
     class Meta:
         model = Post
         fields = [
